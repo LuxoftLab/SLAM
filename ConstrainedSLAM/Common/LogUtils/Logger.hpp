f5fc27c1fb0f91376c25b1b2916b5356b9a09321
Alex Reshetnyk <getupandgo11@gmail.com>
2015-03-17 21:13:58 +0000

    [logger] makros rewrited

--- ConstrainedSLAM/Common/LogUtils/Logger.hpp
+++ ConstrainedSLAM/Common/LogUtils/Logger.hpp
@@ -1,7 +1,7 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
-#include "Private/CLogger.hpp"
+#include "private/CLogger.hpp"
 
 #define CVNAR_LOGGER_INIT                       CLogger& i = CLogger::getInstance();
 #define CVNAR_LOGGER_SET(MODE)                  i.setMode(MODE);
@@ -9,7 +9,7 @@
 #define CVNAR_LOG_ERROR                         i.setDetalizationLevel(ERROR);
 #define CVNAR_LOG_WARNING                       i.setDetalizationLevel(WARNING);
 #define CVNAR_LOG_MESSAGE                       i.setDetalizationLevel(MESSAGE);
-#define CVNAR_LOG_WRITE(PRIORITY)               if ((PRIORITY > i.getLogLevel()) || (i.getMode() == OFF && PRIORITY != ERROR)) ; \
-                                                else i.write(PRIORITY)
+#define CVNAR_LOG_WRITE(PRIORITY)               if (PRIORITY < i.getLogLevel() || PRIORITY == ERROR) i.write(PRIORITY)
+
 #endif // LOGGER_HPP
 
