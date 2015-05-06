a547577ce4b6d160d5aa86ce3577efaab4e5eb41
Alex Reshetnyk <getupandgo11@gmail.com>
2015-05-06 20:40:47 +0000

    [3D scene] implemented saving to obj file

--- ConstrainedSLAM/Scene3D/CScene3D.hpp
+++ ConstrainedSLAM/Scene3D/CScene3D.hpp
@@ -3,7 +3,10 @@
 
 #include <vector>
 #include <map>
+#include <fstream>
 #include <boost/shared_ptr.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 #include "SCamera.hpp"
 #include "SPoint3D.hpp"
@@ -13,13 +16,22 @@ class CScene3D {
 public:
    typedef boost::shared_ptr<SPoint3D> tPointPtr;
    typedef boost::shared_ptr<SCamera> tCameraPtr;
+   typedef boost::numeric::ublas::matrix <double> tMatrixDouble;
 
    tCameraPtr createCamera();
    tPointPtr getPoint(int i);
+   void saveToObj(std::string path);
 
 private:
-   std::map<int, tPointPtr> mPoints;
-   std::vector<tCameraPtr> mCameras;
+   tMatrixDouble quaternionToMatrix(double quaternion[]);
+   void drawCamera(double quaternion[], double position[]);
+   void drawPoint(double center[]);
+
+   std::map <int, tPointPtr> mPoints;
+   std::vector <tCameraPtr> mCameras;
+
+   std::fstream fileStream;
+   unsigned short vertexes = 0;
 };
 
 #endif // CSCENE3D_HPP
